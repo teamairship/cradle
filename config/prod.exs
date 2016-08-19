82578d6d9447b3aee0d261c0070f7e97dc38ac74
Ryan T. Hosford <tad.hosford@gmail.com>
2016-08-19 14:29:50 -0500

    Update app name

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cradle, Cradle.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "secure-shelf-93296.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "harvest-to-cradle.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
